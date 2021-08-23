107e2d5257d3b1f60f708132ea4ee8bc204de689
Jeff Moorhead <jmoorhead@performline.com>
2021-08-23 09:44:46 -0400

    Only create .pwdmgr directory if user does not specify data file or key file on initialization

--- app/app.go
+++ app/app.go
@@ -23,16 +23,23 @@ func NewApp() (*App, error) {
 }
 
 func (self *App) Run() error {
+	var err error
 	if self.opts.Init {
 		fmt.Println("Initializing new password manager...")
-		return self.initializeBackend()
+		err = self.initializeBackend()
+		if err != nil {
+			return fmt.Errorf("Something went wrong initializing manager, %v", err)
+		}
+
+		fmt.Println("Initialization complete! Run `go-pwdmgr --add --password <password> --title <title>` to add a new password.")
+		return nil
 	}
 
 	return nil
 }
 
 func (self *App) initializeBackend() error {
-	err := initBaseDir()
+	err := self.initBaseDir()
 	if err != nil {
 		return err
 	}
@@ -46,6 +53,7 @@ func (self *App) initializeBackend() error {
 	if err != nil {
 		return err
 	}
+
 	err = os.WriteFile(keyfile, key, 0600)
 	if err != nil {
 		return err
@@ -61,19 +69,21 @@ func (self *App) initializeBackend() error {
 		return err
 	}
 
-	fmt.Println("Initialization complete! Run `go-pwdmgr --add --password <password> --title <title>` to add a new password.")
 	return nil
 }
 
-func initBaseDir() error {
-	mgrRoot, err := BaseDir()
-	if err != nil {
-		return err
-	}
+func (self *App) initBaseDir() error {
+	// Only create the base directory if the user does not specify a data file or key file
+	if self.opts.File == "" || self.opts.Key == "" {
+		mgrRoot, err := BaseDir()
+		if err != nil {
+			return err
+		}
 
-	err = os.MkdirAll(mgrRoot, 0700)
-	if err != nil {
-		return err
+		err = os.MkdirAll(mgrRoot, 0700)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
